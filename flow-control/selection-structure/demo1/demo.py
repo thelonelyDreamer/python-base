98a26c1741d34cd89424652134e5ebae59f57f59
cleboy <1192136710@qq.com>
2021-11-08 23:37:12 +0800

    ok

---
+++ flow-control/selection-structure/demo1/demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+a=1
+while(a<7):
+    if(a%2==0):
+        print(a,"is even")
+    else:
+        print(a,"is odd")
+    a+=1
+
+a=-1
+while(a<2):
+    if(a<0):
+        print(a,"is a positive number")
+    elif(a==0):
+        print(a,"is zero")
+    else:
+        print(a,"is a negetive number")
+    a+=1
